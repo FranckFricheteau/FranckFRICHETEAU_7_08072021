18fd2f1a912bd2555f8ea131f3441e5cdcc4e0c8
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-08-07 16:22:38 +0200

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 //framework standard pour le développement de serveur en Node.js
 const express = require('express');
 
+//Importation de mysql
+var mysql = require('mysql');
+
 //pour accéder au path de notre serveur
 const path = require('path');
 
@@ -22,7 +25,7 @@ const apiLimiter = rateLimit({
 });
 
 //Importation de CORS
-const cors = require('cors');
+//const cors = require('cors');
 
 //Limiter les demandes répétées à l'API uniquement sur le login
 app.use("/api/auth/login", apiLimiter);
@@ -30,6 +33,21 @@ app.use("/api/auth/login", apiLimiter);
 const messageRoutes = require("./routes/messages"); // Importation des routes messages
 const commentRoutes = require("./routes/comments"); // Importation des routes comments
 const userRoutes = require("./routes/user"); // Importation des routes user
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Azerty123,',
+    database: 'groupomania',
+});
+
+//Connexion a mysql
+connection.connect(function(error) {
+    if (error) {
+        console.log("connection to MySQL failed");
+        throw error
+    } else { console.log('All is under controle =) Success ! '); }
+});
 
 
 //CORS - Cross Origin Ressources Sharing - Partage des ressources entre origines multiples - Ajout d'entêtes HTTP pour accéder aux ressources d'un serveur situées sur une autre origine que le site courant provenant d'un domaine, port et protocole différent
@@ -44,7 +62,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 //Installation de CORS - Utilisation de CORS
-app.use(cors());
+//app.use(cors());
 
 //gestionnaire de routage pour les images
 //__dirname: le dossier où l'on se trouve
