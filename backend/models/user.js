d9bc3505fa1ad10af0eecc4504b5c59c0b248231
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-08-09 22:14:54 +0200

    update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,106 +3,104 @@ var mysql = require('mysql');
 const db = require('./db');
 const Utils = require('../libs/utils');
 
-const User = function(user) {
+const Users = function(user) {
     this.pseudo = user.pseudo,
         this.email = user.email,
         this.password = user.password,
         this.profilPic = user.profilPic,
         this.isAdmin = !!user.isAdmin,
         this.isActive = !!user.isActive
+
+    // Création d'un user
+    Users.create = (newUser, result) => {
+        let statment = 'INSERT INTO users SET ?';
+        db.query(statment, newUser, (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            } else {
+                result(null, {
+                    id: res.id,
+                    ...newUser
+                })
+            }
+        })
+    };
+
+    // Trouver un user via son email
+    Users.findOne = (email, result) => {
+        let statment = 'SELECT * FROM users WHERE email=? AND isActive=true';
+        db.query('SELECT * FROM users WHERE email=? AND isActive=true', email, (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            }
+            result(null, res[0])
+        })
+    };
+
+    // Trouver un user via son id
+    Users.findOneById = (id, result) => {
+        let statment = 'SELECT * FROM users WHERE id=? AND isActive=true';
+        db.query(statment, id, (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            }
+            result(null, res[0])
+        })
+    };
+
+    // Trouver tous les users dans la BDD
+    Users.findAll = (result) => {
+        let statment = 'SELECT * FROM users WHERE isActive=true';
+        db.query(statment, (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            }
+            result(null, res)
+        })
+    };
+
+    // Modifier le pseudo d'un user
+    Users.modifyPseudo = (user, result) => {
+        let statment = 'UPDATE users SET pseudo=? WHERE id=? AND isActive=true';
+        db.query(statment, user, (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            } else {
+                result(null, res)
+            }
+        })
+    };
+
+    // Modifier la photo de profil d'un user
+    Users.modifyProfilPic = (user, result) => {
+        let statment = 'UPDATE users SET profilPic=? WHERE id=? AND isActive=true';
+        db.query(statment, user, (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            } else {
+                result(null, res)
+            }
+        })
+    };
+
+    // Supprimer un user
+    Users.deactivate = (id, result) => {
+        let statment = 'UPDATE users SET isActive=false WHERE id=?';
+        db.query(statment, id, (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            } else {
+                result(null, res)
+            }
+        })
+    };
+
 }
-
-
-
-// Création d'un user
-User.create = (newUser, result) => {
-    let statment = 'INSERT INTO users SET ?';
-    User.create = (statment, newUser, (err, res) => {
-        if (err) {
-            result(err, null);
-            return;
-        } else {
-            result(null, {
-                id: res.id,
-                ...newUser
-            })
-        }
-    })
-};
-
-// Trouver un user via son email
-User.findOneByEmail = (email, result) => {
-    let statment = 'SELECT * FROM users WHERE email=? AND isActive=true';
-    User.findOneByEmail = (statment, email, (err, res) => {
-        if (err) {
-            result(err, null);
-            return;
-        }
-        result(null, res[0])
-    })
-};
-
-// Trouver un user via son id
-User.findOneById = (id, result) => {
-    let statment = 'SELECT * FROM users WHERE id=? AND isActive=true';
-    User.findOneById = (statment, id, (err, res) => {
-        if (err) {
-            result(err, null);
-            return;
-        }
-        result(null, res[0])
-    })
-};
-
-// Trouver tous les users dans la BDD
-User.findAll = (result) => {
-    let statment = 'SELECT * FROM users WHERE isActive=true';
-    User.findAll = (statment, (err, res) => {
-        if (err) {
-            result(err, null);
-            return;
-        }
-        result(null, res)
-    })
-};
-
-// Modifier le pseudo d'un user
-User.modifyPseudo = (user, result) => {
-    let statment = 'UPDATE users SET pseudo=? WHERE id=? AND isActive=true';
-    User.modifyPseudo = (statment, user, (err, res) => {
-        if (err) {
-            result(err, null);
-            return;
-        } else {
-            result(null, res)
-        }
-    })
-};
-
-// Modifier la photo de profil d'un user
-User.modifyProfilPic = (user, result) => {
-    let statment = 'UPDATE users SET profilPic=? WHERE id=? AND isActive=true';
-    User.modifyProfilPic = (statment, user, (err, res) => {
-        if (err) {
-            result(err, null);
-            return;
-        } else {
-            result(null, res)
-        }
-    })
-};
-
-// Supprimer un user
-User.deactivate = (id, result) => {
-    let statment = 'UPDATE users SET isActive=false WHERE id=?';
-    User.deactivate = (statment, id, (err, res) => {
-        if (err) {
-            result(err, null);
-            return;
-        } else {
-            result(null, res)
-        }
-    })
-};
-
-module.exports = User;
+module.exports = Users;
