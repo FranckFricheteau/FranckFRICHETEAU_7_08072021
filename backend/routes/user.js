b5c3b62eae8e0d183c780c5fe3fe4fdfa9af40f8
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-08-31 17:12:46 +0200

    update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,27 +1,34 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express"); // Importation framework express
+const router = express.Router(); // Création routeur avec la méthode router() d'express
 
-//Importer multer
-const multer = require('../middleware/multer_config');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer_config'); // Importation du middleware multer pour les images
+const admin = require('../middleware/admin_control')
 
 //Importation du controllers user
 const userCtrl = require('../controllers/user');
 
-const admin = require('../middleware/admin_control');
+//importation du middleware passwordValidator
+const passwordValidator = require('../middleware/passwordValidator');
 
-//Importation de la route GET pour afficher tous les utilisateurs
-router.get('users', userCtrl.getAllUsers);
 
-//Importation de la route GET pour afficher un utilisateur
-router.get('/profile/:id', userCtrl.getOneUser);
+//importation des routes signup & login
+router.post('/signup', passwordValidator, userCtrl.signup); //Chiffre le mot de passe de l'utilisateur, ajoute l'utilisateur à la base de données
+router.post('/login', userCtrl.login); //Vérifie les informations d'identification de l'utilisateur, en renvoyant l'identifiant userID depuis la base de données et un jeton Web JSON signé(contenant également l'identifiant userID
 
-//Route PUT pour modifier un utlisateur
-router.put('/profile/:id,', userCtrl.updateOneUserPseudo);
+// Route GET pour afficher tous les utilisateurs
+router.get('/users', userCtrl.getAllUsers);
 
-//Route POST pour modifier la photo de profil d'un utilisateur
-router.post('/profilPic/:id', multer, userCtrl.updateOneUserFile);
+// Route GET pour afficher un user
+router.get('/profile/:id', auth, userCtrl.getOneUser);
 
-//Route DELETE pour supprimer un utilisateur
-router.delete('/users/:id', admin, userCtrl.deactivateUser);
+// Route PUT pour modifier un user
+router.put('/profile/:id', auth, userCtrl.updateOneUserPseudo);
+
+// Route PUT pour modifier un user
+router.post('/profilPic/:id', auth, multer, userCtrl.updateOneUserFile);
+
+// Route DELETE pour supprimer un user
+router.delete('/users/:id', auth, admin, userCtrl.deactivateUser);
 
 module.exports = router;
