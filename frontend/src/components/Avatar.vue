bf3791784733bb0d482cd8f94ea1a47583a7081d
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-09-21 16:19:13 +0200

    update avatar.vue

--- frontend/src/components/Avatar.vue
+++ frontend/src/components/Avatar.vue
@@ -17,7 +17,7 @@ export default {
   computed: {
     avatar() { //photo de l'utilisateur
     if (this.user.profilPic == '' || this.user.profilPic == null) { // compare tous types de photos OU pas de photo de l'utilisateur
-      return '/profile_pic.png' // retourne une image en particulier (profile_pic.png)
+      return '/profile_utilisator.png' // retourne une image en particulier (profile_utilisator.png)
       }
       return 'http://localhost:3000/images/' + this.user.profilPic // retourne sur le dossier images du port 3000 avec la photo de profil de l'utilisateur
     }
