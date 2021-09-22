165b8f93e53e1aab9a74f2773707a0d037087b71
FranckFricheteau <50356262+FranckFricheteau@users.noreply.github.com>
2021-09-22 15:21:33 +0200

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes 
-    max: 60 // 60 essais max
+    max: 30 // 30 essais max
 });
 
 //import des routeurs dans l'application
